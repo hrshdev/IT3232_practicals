2f30494fd2e24d2345fd4d291b45cfc2b793e028
hrsh_dev <vadivelharson123@gmail.com>
2025-04-04 09:29:55 +0530

    Added CRUD Operations

--- day2/myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ day2/myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -100,7 +100,7 @@ public class AppController {
 	    for (int i = 0; i < n - 1; i++) {
 	        for (int j = 0; j < n - i - 1; j++) {
 	            if (students.get(j).getGpa() < students.get(j + 1).getGpa()) {
-	                // Swap students[j] and students[j+1]
+
 	                Student temp = students.get(j);
 	                students.set(j, students.get(j + 1));
 	                students.set(j + 1, temp);
